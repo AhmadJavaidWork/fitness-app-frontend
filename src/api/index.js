78d8f386cd63b005301c4f71ea272c09bbf2f550
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2021-04-14 10:38:57 +0500

    changed production api url

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,7 @@ const Bearer = JSON.parse(localStorage.getItem('token'));
 axios.defaults.withCredentials = true;
 
 if (config.env === 'production') {
-  axios.defaults.baseURL = 'https://fitness-app-backend.herokuapp.com/';
+  axios.defaults.baseURL = 'https://fitness-app-backends.herokuapp.com/';
 } else {
   axios.defaults.baseURL = 'http://localhost:9000/';
 }
